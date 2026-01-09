85c4a738dc5cac38fee4af84e2a39808e7db9d1f
SheriHarari <sherryperets@gmail.com>
2026-01-09 11:38:25 -0800

    update APN binding data and list of filtered accession numbers

--- run_snakemake_cluster.bash
+++ run_snakemake_cluster.bash
@@ -23,8 +23,7 @@ snakemake \
     --latency-wait 60 \
     --restart-times 2 \
     --rerun-triggers mtime \
-    --rerun-incomplete \
-    --forceall
+    --rerun-incomplete 
 
 # Signal that snakemake has complete
 echo "Run of snakemake complete."
